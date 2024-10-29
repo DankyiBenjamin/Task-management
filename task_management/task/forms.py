db072392fe571c7b771f40cfb1672f22ed2701c6
“Dankyi <dankyibenjamin0@gmail.com>
2024-10-29 14:40:22 +0000

    creating Categories

--- task_management/task/forms.py
+++ task_management/task/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Task
+from .models import Task, Category
 
 
 class TaskForm(forms.ModelForm):
@@ -7,8 +7,20 @@ class TaskForm(forms.ModelForm):
         input_formats=['%Y-%m-%d'],  # Specify your desired date format here
         widget=forms.DateInput(attrs={'type': 'date'})  # HTML5 date picker
     )
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.none(),
+        required=False
+    )
 
     class Meta:
         model = Task
         fields = ['title', 'description', 'status',
                   'due_date', 'priority', 'category']
+
+    def __init__(self, *args, **kwargs):
+        # Get the user from view
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+        if user:
+            self.fields['category'].queryset = Category.objects.filter(
+                user=user)
--- task_management/task/models.py
+++ task_management/task/models.py
@@ -3,8 +3,18 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+# defining the categories model
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 # defining the Task model
+
+
 class Task(models.Model):
     STATUS_CHOICES = [
         ('todo', 'To Do'),
@@ -26,7 +36,8 @@ class Task(models.Model):
     due_date = models.DateField(null=True, blank=True)
     priority = models.CharField(
         max_length=10, choices=PRIORITY_CHOICES, default='medium')
-    category = models.CharField(max_length=50, blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True)
     # Each task belongs to a user
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
