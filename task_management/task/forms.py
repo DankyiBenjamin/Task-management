74490cb0eee4b66abe6dfffddb9a204dbcf51ab9
“Dankyi <dankyibenjamin0@gmail.com>
2024-10-29 14:19:22 +0000

    fixing the date format

--- task_management/task/forms.py
+++ task_management/task/forms.py
@@ -3,6 +3,11 @@ from .models import Task
 
 
 class TaskForm(forms.ModelForm):
+    due_date = forms.DateField(
+        input_formats=['%Y-%m-%d'],  # Specify your desired date format here
+        widget=forms.DateInput(attrs={'type': 'date'})  # HTML5 date picker
+    )
+
     class Meta:
         model = Task
         fields = ['title', 'description', 'status',
